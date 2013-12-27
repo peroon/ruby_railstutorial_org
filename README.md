59f3e055975862a099f0440adad3cc06ca698bdd
Keita Ishibashi <fromjpn@gmail.com>
2013-12-28 02:04:00 +0900

    first (-1.2.5)

---
+++ README.md
@@ -0,0 +1,6 @@
+ruby.railstutorial.org
+
+(Japanese version)
+http://railstutorial.jp/?version=4.0#top
+
+Finish the tutorial at one day. (2013/12/28)
---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '72f18a4a561a88ccfb35390e4440012f4d2d2caf01176c9b2393e028492e4f0b576dc3599a73339f4bcda327aaa48379ef6a9461e98cbf0c0bb7327f9ee9949e'
